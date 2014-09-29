2c4bf5034844987e93438a316c15b0721e44f0a0
Timofey Titovets <nefelim4ag@gmail.com>
2014-09-29 10:20:41 +0300

    fix: compile with optimizations
    Signed-off-by: Timofey Titovets <nefelim4ag@gmail.com>

--- debug.h
+++ debug.h
@@ -2,6 +2,7 @@
 #define	__DEBUG_H__
 
 #include <stdio.h>
+#include <stdbool.h>
 
 extern int verbose;
 extern int debug;
@@ -72,6 +73,45 @@ void print_mem_stats(void);
 			abort();					\
 		}							\
 	} while(0)
+
+
+/*
+ * compiletime_assert and associated code taken from
+ * linux-3.14.git/include/linux/compiler.h
+ */
+
+# define __compiletime_warning(message)
+# define __compiletime_error(message)
+
+/*
+ * Sparse complains of variable sized arrays due to the temporary variable in
+ * __compiletime_assert. Unfortunately we can't just expand it out to make
+ * sparse see a constant array size without breaking compiletime_assert on old
+ * versions of GCC (e.g. 4.2.4), so hide the array from sparse altogether.
+ */
+#define __compiletime_error_fallback(condition) \
+	do { ((void)sizeof(char[1 - 2 * condition])); } while (0)
+
+#define __compiletime_assert(condition, msg, prefix, suffix)		\
+	do {								\
+		bool __cond = !(condition);				\
+		extern void prefix ## suffix(void) __compiletime_error(msg); \
+		if (__cond)						\
+			prefix ## suffix();				\
+		__compiletime_error_fallback(__cond);			\
+	} while (0)
+
+/**
+ * compiletime_assert - break build and emit msg if condition is false
+ * @condition: a compile-time constant condition to check
+ * @msg:       a message to emit if condition is false
+ *
+ * In tradition of POSIX assert, this macro will break the build if the
+ * supplied condition is *false*, emitting the supplied error message if the
+ * compiler has support to do so.
+ */
+#define compiletime_assert(condition, msg) \
+	__compiletime_assert(condition, msg, __compiletime_assert_, __LINE__)
 
 /*
  * BUILD_BUG_ON() and associated code taken from
