markfasheh/duperemove

commit 5497f7e17cb2102e73a1d68c4685d9a8c58ace46

Keep file blocks on per blocklist/filerec list
We have a performance issue when visiting very large buckets - walking
hundreds of thousands of hashes can take a lot of time. Previously
this was solved by pushing large buckets to an alternative algorithm. This had
a couple problems, the most obvious being how to define a 'very large
bucket'. It also didn't attempt solve the underlying problem.

A better solution is to have each dupe_blocks_list maintain a tree of
per-filerec hash blocks. The extent finding code then can walk the
per-filerec list of blocks, thus avoiding visiting any nodes that it
won't care about.

This is also much faster than the previous solution. The time for an
extent search from hashfile of my home directory (~750 gigs) dropped
from 55 minutes to 22 minutes.

Signed-off-by: Mark Fasheh <mfasheh@suse.de>

duperemove.c
@@ -927,23 +927,27 @@ out:
 }
 
 /*
- * Start and extent search at each block in our dups list which is
- * owned by walk_file.
+ * Start an extent search (with orig_block) at each block in our dups
+ * list which is owned by walk_file.
  */
-static void walk_each_shared_dupe(struct file_block *orig_block,
-				  struct filerec *walk_file,
-				  struct results_tree *res)
+static void lookup_walk_file_hash_head(struct file_block *orig_block,
+				       struct filerec *walk_file,
+				       struct results_tree *res)
 {
 	struct dupe_blocks_list *parent = orig_block->b_parent;
 	struct file_block *cur;
+	struct file_hash_head *head = find_file_hash_head(parent, walk_file);
 
-	list_for_each_entry(cur, &parent->dl_list, b_list) {
-		/* Ignore self and any blocks from another file */
+	/* find_file_dups should have checked this for us already */
+	abort_on(head == NULL);
+
+	list_for_each_entry(cur, &head->h_blocks, b_head_list) {
+		/* Ignore self. Technically this shouldn't happen (see above)
+		 * until we allow walking a file against itself. */
 		if (cur == orig_block)
 			continue;
 
-		if (cur->b_file != walk_file)
-			continue;
+		abort_on(cur->b_file != walk_file);
 
 		if (walk_dupe_block(orig_block->b_file, orig_block,
 				    walk_file, cur, res))
@@ -959,12 +963,16 @@ static void find_file_dupes(struct filerec *file, struct filerec *walk_file,
 	list_for_each_entry(cur, &file->block_list, b_file_next) {
 		if (block_seen(cur))
 			continue;
+
+		if (!file_in_dups_list(cur->b_parent, walk_file))
+			continue;
+
 		/*
 		 * For each file block with the same hash:
 		 *  - Traverse, along with original file until we have no match
 		 *     - record
 		 */
-		walk_each_shared_dupe(cur, walk_file, res);
+		lookup_walk_file_hash_head(cur, walk_file, res);
 	}
 	clear_all_seen_blocks();
 }
@@ -975,50 +983,6 @@ static int compare_files(struct results_tree *res, struct filerec *file1, struct
 	find_file_dupes(file1, file2, res);
 
 	return mark_filerecs_compared(file1, file2);
-}
-
-/*
- * This dupe list is too large for the extent search algorithm to
- * handle efficiently. Instead of walking the block list, we walk the
- * list of files referenced and compare them to each other directly.
- *
- * A future improvement might be to always do this, at the cost of
- * extra memory usage.
- */
-static int walk_large_dups(struct hash_tree *tree,
-			   struct results_tree *res,
-			   struct dupe_blocks_list *dups)
-{
-	int ret;
-	struct rb_node *node = rb_first(&dups->dl_files_root);
-	struct rb_node *next;
-	struct filerec_token *t1, *t2;
-	struct filerec *file1, *file2;
-
-	while (node) {
-		t1 = rb_entry(node, struct filerec_token, t_node);
-		file1 = t1->t_file;
-
-		next = rb_next(node);
-		while (next) {
-			t2 = rb_entry(next, struct filerec_token, t_node);
-			file2 = t2->t_file;
-
-			/* filerec token tree does not allow duplicates */
-			abort_on(file1 == file2);
-			if (!filerecs_compared(file1, file2)) {
-				ret = compare_files(res, file1, file2);
-				if (ret)
-					return ret;
-				break;
-			}
-
-			next = rb_next(next);
-		}
-		node = rb_next(node);
-	}
-
-	return 0;
 }
 
 static int walk_dupe_hashes(struct dupe_blocks_list *dups,
@@ -1085,7 +1049,6 @@ static int find_all_dups(struct hash_tree *tree, struct results_tree *res)
 	struct rb_root *root = &tree->root;
 	struct rb_node *node = rb_first(root);
 	struct dupe_blocks_list *dups;
-	LIST_HEAD(large_dupes);
 
 	while (1) {
 		if (node == NULL)
@@ -1093,26 +1056,11 @@ static int find_all_dups(struct hash_tree *tree, struct results_tree *res)
 
 		dups = rb_entry(node, struct dupe_blocks_list, dl_node);
 
-		if (dups->dl_num_files) {
-			list_add_tail(&dups->dl_large_list, &large_dupes);
-
-			printf("Hash (\"");
-			debug_print_digest(stdout, dups->dl_hash);
-			printf("\") has %u items (across %u files), processing later.\n",
-			       dups->dl_num_elem, dups->dl_num_files);
-		} else if (dups->dl_num_elem > 1) {
-			ret = walk_dupe_hashes(dups, res);
-			if (ret)
-				return ret;
-		}
-
-		node = rb_next(node);
-	}
-
-	list_for_each_entry(dups, &large_dupes, dl_large_list) {
-		ret = walk_large_dups(tree, res, dups);
+		ret = walk_dupe_hashes(dups, res);
 		if (ret)
 			return ret;
+
+		node = rb_next(node);
 	}
 
 	return 0;

debug.h
@@ -57,6 +57,7 @@ declare_alloc_tracking_header(extent);
 declare_alloc_tracking_header(filerec);
 declare_alloc_tracking_header(files_compared);
 declare_alloc_tracking_header(filerec_token);
+declare_alloc_tracking_header(file_hash_head);
 /* Can be called anywhere we want to dump the above statistics */
 void print_mem_stats(void);
 

filerec.c
@@ -83,13 +83,19 @@ void filerec_token_free(struct filerec_token *token)
 		free_filerec_token(token);
 }
 
+static void filerec_token_init(struct filerec_token *token,
+			       struct filerec *file)
+{
+	rb_init_node(&token->t_node);
+	token->t_file = file;
+}
+
 struct filerec_token *filerec_token_new(struct filerec *file)
 {
 	struct filerec_token *token = malloc_filerec_token();
 
 	if (token) {
-		rb_init_node(&token->t_node);
-		token->t_file = file;
+		filerec_token_init(token, file);
 	}
 	return token;
 }

hash-tree.c
@@ -34,48 +34,83 @@
 declare_alloc_tracking(file_block);
 declare_alloc_tracking(dupe_blocks_list);
 
-static int add_one_filerec_token(struct dupe_blocks_list *dups,
-				 struct filerec *file)
+struct file_hash_head *find_file_hash_head(struct dupe_blocks_list *dups,
+					   struct filerec *file)
 {
-	struct filerec_token *t = NULL;
+	struct rb_node *n = dups->dl_files_root.rb_node;
+	struct file_hash_head *head;
 
-	if (find_filerec_token_rb(&dups->dl_files_root, file))
-		return 0;
+	while (n) {
+		head = rb_entry(n, struct file_hash_head, h_node);
 
-	t = filerec_token_new(file);
-	if (!t)
+		if (head->h_file < file)
+			n = n->rb_left;
+		else if (head->h_file > file)
+			n = n->rb_right;
+		else return head;
+	}
+	return NULL;
+}
+
+static void insert_file_hash_head(struct dupe_blocks_list *dups,
+				  struct file_hash_head *head)
+{
+	struct rb_node **p = &dups->dl_files_root.rb_node;
+	struct rb_node *parent = NULL;
+	struct file_hash_head *tmp;
+
+	while (*p) {
+		parent = *p;
+
+		tmp = rb_entry(parent, struct file_hash_head, h_node);
+
+		if (tmp->h_file < head->h_file)
+			p = &(*p)->rb_left;
+		else if (tmp->h_file > head->h_file)
+			p = &(*p)->rb_right;
+		else abort_lineno(); /* We should never find a duplicate */
+	}
+
+	rb_link_node(&head->h_node, parent, p);
+	rb_insert_color(&head->h_node, &dups->dl_files_root);
+}
+
+static int add_file_hash_head(struct dupe_blocks_list *dups,
+			      struct file_block *block)
+{
+	struct filerec *file = block->b_file;
+	struct file_hash_head *head = find_file_hash_head(dups, file);
+
+	if (head)
+		goto add;
+
+	head = malloc(sizeof(*head));
+	if (!head)
 		return ENOMEM;
 
-	insert_filerec_token_rb(&dups->dl_files_root, t);
+	head->h_file = file;
+	rb_init_node(&head->h_node);
+	INIT_LIST_HEAD(&head->h_blocks);
+	insert_file_hash_head(dups, head);
 	dups->dl_num_files++;
+add:
+	/* We depend on this being added in increasing block order */
+	list_add_tail(&block->b_head_list, &head->h_blocks);
 	return 0;
 }
 
-static int add_filerec_tokens(struct dupe_blocks_list *dups)
+static void free_one_hash_head(struct dupe_blocks_list *dups,
+			       struct file_hash_head *head)
 {
-	struct file_block *block;
-
-	list_for_each_entry(block, &dups->dl_list, b_list) {
-		if (add_one_filerec_token(dups, block->b_file))
-			return ENOMEM;
-	}
-	return 0;
+	rb_erase(&head->h_node, &dups->dl_files_root);
+	free(head);
 }
 
-static void free_filerec_tokens(struct dupe_blocks_list *dups)
+int file_in_dups_list(struct dupe_blocks_list *dups, struct filerec *file)
 {
-	struct rb_node *node = rb_first(&dups->dl_files_root);
-	struct filerec_token *t;
-
-	while (node) {
-		t = rb_entry(node, struct filerec_token, t_node);
-
-		node = rb_next(node);
-
-		dups->dl_num_files--;
-		rb_erase(&t->t_node, &dups->dl_files_root);
-		filerec_token_free(t);
-	}
+	if (find_file_hash_head(dups, file))
+		return 1;
+	return 0;
 }
 
 static void insert_block_list(struct hash_tree *tree,
@@ -147,29 +182,26 @@ int insert_hashed_block(struct hash_tree *tree,	unsigned char *digest,
 		rb_init_node(&d->dl_node);
 		rb_init_node(&d->dl_by_size);
 		INIT_LIST_HEAD(&d->dl_list);
-		INIT_LIST_HEAD(&d->dl_large_list);
 		d->dl_files_root = RB_ROOT;
 
 		insert_block_list(tree, d);
-	}
-
-	if (d->dl_num_elem >= DUPLIST_CONVERT_LIMIT && d->dl_num_files == 0) {
-		if (add_filerec_tokens(d))
-			return ENOMEM;
 	}
 
 	e->b_file = file;
 	e->b_seen = 0;
 	e->b_loff = loff;
 	e->b_flags = flags;
-	list_add_tail(&e->b_file_next, &file->block_list);
-	file->num_blocks++;
 	e->b_parent = d;
 
-	if (d->dl_num_files) {
-		if (add_one_filerec_token(d, file))
-			return ENOMEM;
+	INIT_LIST_HEAD(&e->b_head_list);
+
+	if (add_file_hash_head(d, e)) {
+		free_file_block(e);
+		return ENOMEM;
 	}
+
+	list_add_tail(&e->b_file_next, &file->block_list);
+	file->num_blocks++;
 
 	d->dl_num_elem++;
 	list_add_tail(&e->b_list, &d->dl_list);
@@ -182,6 +214,7 @@ static void remove_hashed_block(struct hash_tree *tree,
 				struct file_block *block, struct filerec *file)
 {
 	struct dupe_blocks_list *blocklist = block->b_parent;
+	struct file_hash_head *head;
 
 	abort_on(blocklist->dl_num_elem == 0);
 
@@ -193,12 +226,16 @@ static void remove_hashed_block(struct hash_tree *tree,
 	list_del(&block->b_file_next);
 	list_del(&block->b_list);
 
+	list_del(&block->b_head_list);
+	head = find_file_hash_head(blocklist, file);
+	if (head && list_empty(&head->h_blocks))
+		free_one_hash_head(blocklist, head);
+
 	blocklist->dl_num_elem--;
 	if (blocklist->dl_num_elem == 0) {
 		rb_erase(&blocklist->dl_node, &tree->root);
 		tree->num_hashes--;
 
-		free_filerec_tokens(blocklist);
 		free_dupe_blocks_list(blocklist);
 	}
 

hash-tree.h
@@ -14,21 +14,11 @@ struct dupe_blocks_list {
 	unsigned int	dl_num_elem;
 	struct list_head	dl_list;
 
-	/*
-	 * num_files and files_root are used when the total number of
-	 * blocks in the list exceeds DUPLIST_CONVERT_LIMIT (defined
-	 * below)
-	 */
 	unsigned int		dl_num_files;
-	struct rb_root		dl_files_root;
-	struct list_head	dl_large_list; /* Temporary list for
-						* use by extent finding code */
+	struct rb_root		dl_files_root; /* stores file_hash_head nodes */
 
 	unsigned char		dl_hash[DIGEST_LEN_MAX];
 };
-
-/* Max number of blocks before we'll add filerec tokens */
-#define	DUPLIST_CONVERT_LIMIT		30000
 
 /* Fiemap flags that would cause us to skip comparison of the block */
 #define FIEMAP_SKIP_FLAGS	(FIEMAP_EXTENT_UNKNOWN|FIEMAP_EXTENT_DATA_INLINE|FIEMAP_EXTENT_UNWRITTEN)
@@ -50,11 +40,33 @@ struct file_block {
 					  * with this md5. */
 
 	struct list_head	b_file_next; /* filerec->block_list */
+
+	struct list_head	b_head_list; /* file_hash_head->h_blocks */
 };
 
 int insert_hashed_block(struct hash_tree *tree, unsigned char *digest,
 			struct filerec *file, uint64_t loff, unsigned int flags);
 void remove_hashed_blocks(struct hash_tree *tree, struct filerec *file);
+
+/*
+ * Stores a list of blocks with the same hash / filerec
+ * combination. Each dupe_blocks_list keeps a tree of these (sorted by
+ * file).
+ *
+ * This speeds up the extent search by allowing us to skip blocks that
+ * don't belong to the file we are 'walking'. Blocks are inserted into
+ * h_blocks in the same order they are given to insert_hashed_block()
+ * (that is, in order of increasing offset).
+ */
+struct file_hash_head {
+	struct filerec *h_file;
+	struct rb_node h_node;
+	struct list_head h_blocks;
+};
+
+int file_in_dups_list(struct dupe_blocks_list *dups, struct filerec *file);
+struct file_hash_head *find_file_hash_head(struct dupe_blocks_list *dups,
+					   struct filerec *file);
 
 int block_seen(struct file_block *block);
 int block_ever_seen(struct file_block *block);

hashstats.c
@@ -119,6 +119,8 @@ static void print_by_size(void)
 	else
 		printf("Print top %d hashes\n", num_to_print);
 
+	printf("Hash, # Blocks, # Files\n");
+
 	while (1) {
 		if (node == NULL)
 			break;
@@ -126,7 +128,7 @@ static void print_by_size(void)
 		dups = rb_entry(node, struct dupe_blocks_list, dl_by_size);
 
 		debug_print_digest(stdout, dups->dl_hash);
-		printf(", %u\n", dups->dl_num_elem);
+		printf(", %u, %u\n", dups->dl_num_elem, dups->dl_num_files);
 		if (print_blocks) {
 			list_for_each_entry(block, &dups->dl_list,
 					    b_list) {

serialize.c
@@ -34,8 +34,8 @@
 #include "debug.h"
 
 #include "csum.h"
-#include "hash-tree.h"
 #include "filerec.h"
+#include "hash-tree.h"
 
 #include "serialize.h"
 

commit 9e760715cc08825b18fed43e5f7b7c89f2adafb9

Only bother walking a duplist if it has more than one element
Commit 5497f7e17cb2102e73a1d68c4685d9a8c58ace46 accidentally removed this
little bit of logic.

Signed-off-by: Mark Fasheh <mfasheh@suse.de>

duperemove.c
@@ -1109,9 +1109,11 @@ static int find_all_dups(struct hash_tree *tree, struct results_tree *res)
 
 		update_extent_search_status(tree, processed);
 
-		ret = walk_dupe_hashes(dups, res);
-		if (ret)
-			goto out;
+		if (dups->dl_num_elem > 1) {
+			ret = walk_dupe_hashes(dups, res);
+			if (ret)
+				goto out;
+		}
 
 		processed++;
 