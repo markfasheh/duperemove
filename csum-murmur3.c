ab5fd6d3b9604016a57ad4d479cb77ac02c69c0b
jack <alex@jack.fr.eu.org>
2014-11-12 16:25:17 +0100

    - Few coding style fixes
    Signed-off-by: Jack Kenrick <github@jack.fr.eu.org>
    Signed-off-by: Mark Fasheh <mfasheh@suse.de>

--- csum-murmur3.c
+++ csum-murmur3.c
@@ -78,7 +78,7 @@ int init_hash(void)
 {
 	digest_len = 16;
 	abort_on(digest_len == 0 || digest_len > DIGEST_LEN_MAX);
-	return(0);
+	return 0;
 }
 
 void debug_print_digest(FILE *stream, unsigned char *digest)
@@ -107,7 +107,7 @@ struct running_checksum *start_running_checksum(void)
 	struct running_checksum *c = calloc(1, sizeof(struct running_checksum));
 
 	if (c) {
-		//Init h1 & h2 with the same seed
+		/* Init h1 & h2 with the same seed */
 		c->h1 = 42;
 		c->h2 = 42;
 		c->len = 0;
@@ -124,8 +124,10 @@ void add_to_running_checksum(struct running_checksum *c,
 
 	c->len += len;
 
-	//FIXME: Dunno if extra data will works (ie add n * 16 + x bytes,
-	//then m * 16 + y bytes): will the hash work ?
+	/*
+	 * FIXME: Dunno if extra data will works (ie add n * 16 + x bytes,
+	 * then m * 16 + y bytes): will the hash work ?
+	 */
 	assert(nblocks * 16 == len);
 
 	int i;
@@ -135,7 +137,7 @@ void add_to_running_checksum(struct running_checksum *c,
 
 	const uint64_t * blocks = (const uint64_t *)(data);
 
-	for(i = 0; i < nblocks; i++){
+	for (i = 0; i < nblocks; i++) {
 		uint64_t k1 = blocks[i * 2];
 		uint64_t k2 = blocks[i * 2 + 1];
 
@@ -158,41 +160,58 @@ void add_to_running_checksum(struct running_checksum *c,
 		c->h2 = c->h2 * 5 + 0x38495ab5;
 	}
 
-	//FIXME: the code below is used to hash tailing data
-	//Remove the assert up there and uncomment this
-//	const uint8_t * tail = (const uint8_t*)(data + nblocks * 16);
-//
-//	uint64_t k1 = 0;
-//	uint64_t k2 = 0;
-//
-//	switch(len & 15){
-//	case 15: k2 ^= (uint64_t)(tail[14]) << 48;
-//	case 14: k2 ^= (uint64_t)(tail[13]) << 40;
-//	case 13: k2 ^= (uint64_t)(tail[12]) << 32;
-//	case 12: k2 ^= (uint64_t)(tail[11]) << 24;
-//	case 11: k2 ^= (uint64_t)(tail[10]) << 16;
-//	case 10: k2 ^= (uint64_t)(tail[ 9]) << 8;
-//	case  9:
-//		k2 ^= (uint64_t)(tail[ 8]) << 0;
-//		k2 *= c2;
-//		k2 = ROTL64(k2, 33);
-//		k2 *= c1;
-//		c->h2 ^= k2;
-//
-//	case  8: k1 ^= (uint64_t)(tail[ 7]) << 56;
-//	case  7: k1 ^= (uint64_t)(tail[ 6]) << 48;
-//	case  6: k1 ^= (uint64_t)(tail[ 5]) << 40;
-//	case  5: k1 ^= (uint64_t)(tail[ 4]) << 32;
-//	case  4: k1 ^= (uint64_t)(tail[ 3]) << 24;
-//	case  3: k1 ^= (uint64_t)(tail[ 2]) << 16;
-//	case  2: k1 ^= (uint64_t)(tail[ 1]) << 8;
-//	case  1:
-//		k1 ^= (uint64_t)(tail[ 0]) << 0;
-//		k1 *= c1;
-//		k1 = ROTL64(k1, 31);
-//		k1 *= c2;
-//		c->h1 ^= k1;
-//	};
+	/*
+	 * FIXME: the code below is used to hash tailing data
+	 * Remove the assert up there and uncomment the code below
+	 */
+	/*
+	 * const uint8_t * tail = (const uint8_t*)(data + nblocks * 16);
+
+	 * uint64_t k1 = 0;
+	 * uint64_t k2 = 0;
+
+	 * switch(len & 15){
+	 * case 15:
+	 * 	k2 ^= (uint64_t)(tail[14]) << 48;
+	 * case 14:
+	 * 	k2 ^= (uint64_t)(tail[13]) << 40;
+	 * case 13:
+	 * 	k2 ^= (uint64_t)(tail[12]) << 32;
+	 * case 12:
+	 * 	k2 ^= (uint64_t)(tail[11]) << 24;
+	 * case 11:
+	 * 	k2 ^= (uint64_t)(tail[10]) << 16;
+	 * case 10:
+	 * 	k2 ^= (uint64_t)(tail[ 9]) << 8;
+	 * case  9:
+	 * 	k2 ^= (uint64_t)(tail[ 8]) << 0;
+	 * 	k2 *= c2;
+	 * 	k2 = ROTL64(k2, 33);
+	 * 	k2 *= c1;
+	 * 	c->h2 ^= k2;
+
+	 * case  8:
+	 * 	k1 ^= (uint64_t)(tail[ 7]) << 56;
+	 * case  7:
+	 * 	k1 ^= (uint64_t)(tail[ 6]) << 48;
+	 * case  6:
+	 * 	k1 ^= (uint64_t)(tail[ 5]) << 40;
+	 * case  5:
+	 * 	k1 ^= (uint64_t)(tail[ 4]) << 32;
+	 * case  4:
+	 * 	k1 ^= (uint64_t)(tail[ 3]) << 24;
+	 * case  3:
+	 * 	k1 ^= (uint64_t)(tail[ 2]) << 16;
+	 * case  2:
+	 * 	k1 ^= (uint64_t)(tail[ 1]) << 8;
+	 * case  1:
+	 * 	k1 ^= (uint64_t)(tail[ 0]) << 0;
+	 * 	k1 *= c1;
+	 * 	k1 = ROTL64(k1, 31);
+	 * 	k1 *= c2;
+	 * 	c->h1 ^= k1;
+	 * };
+	 */
 }
 
 void finish_running_checksum(struct running_checksum *c, unsigned char *digest)
