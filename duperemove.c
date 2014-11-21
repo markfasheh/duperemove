53f4337725c496209aa8648d1c3b8ec31ffa68f8
Mark Fasheh <mfasheh@suse.de>
2014-11-21 14:29:25 -0800

    Take some of the less obvious options out of usage() and point folks to the man page for them instead. This makes the output of 'duperemove --help' more concise while also giving users a pointer to where they can read a lot more about duperemove.
    Signed-off-by: Mark Fasheh <mfasheh@suse.de>

--- duperemove.c
+++ duperemove.c
@@ -597,16 +597,9 @@ static void usage(const char *prog)
 	printf("\t-h\t\tPrint numbers in human-readable format.\n");
 	printf("\t-x\t\tDon't cross filesystem boundaries.\n");
 	printf("\t-v\t\tBe verbose.\n");
-	printf("\t--hash-threads=N\n\t\t\tUse N threads for hashing phase. "
-	       "Default is automatically detected.\n");
-	printf("\t--read-hashes=hashfile\n\t\t\tRead hashes from a hashfile. "
-	       "A file list is not required with this option.\n");
-	printf("\t--write-hashes=hashfile\n\t\t\tWrite hashes to a hashfile. "
-	       "These can be read in at a later date and deduped from.\n");
-	printf("\t--lookup-extents=[yes|no]\n\t\t\tLookup extent info during "
-	       "checksum phase. Defaults to no.\n");
 	printf("\t--debug\t\tPrint debug messages, forces -v if selected.\n");
 	printf("\t--help\t\tPrints this help text.\n");
+	printf("\nPlease see the duperemove(8) manpage for more options.\n");
 }
 
 static int add_file(const char *name, int dirfd);
