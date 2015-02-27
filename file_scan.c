7c244aeb84ee11c150d93f36b189b03288762e65
Jack Kenrick <github@jack.fr.eu.org>
2015-02-27 14:31:20 +0100

    Split populate_tree_* and csum_whole_file_* code
    Signed-off-by: Jack Kenrick <github@jack.fr.eu.org>

--- file_scan.c
+++ file_scan.c
@@ -42,6 +42,7 @@
 #include "serialize.h"
 #include "file_scan.h"
 #include "bloom.h"
+#include "d_tree.h"
 
 static char path[PATH_MAX] = { 0, };
 static char *pathp = path;
@@ -51,14 +52,12 @@ static dev_t one_fs_dev = 0;
 static uint64_t walked_size = 0;
 
 struct thread_params {
-	struct hash_tree *tree; /* Contains all hashes (all-in-memory path)
-				 * or just the "dups" hashes (swap-file path
-				 */
-	int num_files;		/* Total number of files we hashe */
-	int num_hashes;		/* Total number of hashes we hashed */
-	unsigned int bloom_match; /* Total number of matched by bloom */
-	int hfile;		/* fd to the swap-file, or -1 if none */
-	struct bloom bloom;	/* the real bloom filter */
+	struct rb_root *tree;    /* Unique hashes */
+	int num_files;           /* Total number of files we hashed */
+	int num_hashes;          /* Total number of hashes we hashed */
+	unsigned int bloom_match;/* Total number of matched by bloom */
+	int hfile;               /* fd to the swap-file, or -1 if none */
+	struct bloom bloom;      /* the real bloom filter */
 };
 
 static int walk_dir(const char *name)
@@ -229,9 +228,178 @@ struct block {
 	unsigned char	digest[DIGEST_LEN_MAX];
 };
 
-static void csum_whole_file(struct filerec *file, struct thread_params *params)
+static void csum_whole_file(struct filerec *file, struct hash_tree *tree)
 {
-	struct hash_tree *tree = params->tree;
+	uint64_t off = 0;
+	ssize_t bytes = 0, bytes_read = 0;
+	int ret = 0;
+	struct fiemap_ctxt *fc = NULL;
+	unsigned int flags, hole;
+
+	char *buf = malloc(blocksize);
+	assert(buf != NULL);
+	unsigned char *digest = malloc(DIGEST_LEN_MAX);
+	assert(digest != NULL);
+	static long long unsigned cur_num_filerecs = 0;
+
+	GMutex *mutex = g_dataset_get_data(tree, "mutex");
+
+	__sync_add_and_fetch(&cur_num_filerecs, 1);
+	printf("csum: %s \t[%llu/%llu] (%.2f%%)\n", file->filename,
+	       cur_num_filerecs, num_filerecs,
+		(double)cur_num_filerecs / (double)num_filerecs * 100);
+
+	if (do_lookup_extents) {
+		fc = alloc_fiemap_ctxt();
+		if (fc == NULL) /* This should be non-fatal */
+			fprintf(stderr,
+				"Low memory allocating fiemap context for \"%s\"\n",
+				file->filename);
+	}
+
+	ret = filerec_open(file, 0);
+	if (ret)
+		goto err_noclose;
+
+	while (1) {
+		bytes_read = read(file->fd, buf+bytes, blocksize-bytes);
+		if (bytes_read < 0) {
+			ret = errno;
+			fprintf(stderr, "Unable to read file %s: %s\n",
+				file->filename, strerror(ret));
+			goto err;
+		}
+
+		/* Handle EOF */
+		if (bytes_read == 0)
+			break;
+
+		bytes += bytes_read;
+
+		/* Handle partial read */
+		if (bytes_read > 0 && bytes < blocksize)
+			continue;
+
+		flags = hole = 0;
+		if (fc) {
+			unsigned int fieflags = 0;
+
+			ret = fiemap_iter_get_flags(fc, file, off, &fieflags,
+						    &hole);
+			if (ret) {
+				fprintf(stderr,
+					"Fiemap error %d while scanning file "
+					"\"%s\": %s\n", ret, file->filename,
+					strerror(ret));
+
+				free(fc);
+				fc = NULL;
+			} else {
+				if (hole)
+					flags |= FILE_BLOCK_HOLE;
+				if (fieflags & FIEMAP_SKIP_FLAGS)
+					flags |= FILE_BLOCK_SKIP_COMPARE;
+				if (fieflags & FIEMAP_DEDUPED_FLAGS)
+					flags |= FILE_BLOCK_DEDUPED;
+			}
+		}
+
+		checksum_block(buf, bytes, digest);
+
+		g_mutex_lock(mutex);
+		ret = insert_hashed_block(tree, digest, file, off, flags);
+		g_mutex_unlock(mutex);
+		if (ret)
+			break;
+
+		off += bytes;
+		bytes = 0;
+	}
+
+	filerec_close(file);
+	free(digest);
+	free(buf);
+	if (fc)
+		free(fc);
+
+	return;
+
+err:
+	filerec_close(file);
+err_noclose:
+	free(digest);
+	free(buf);
+	if (fc)
+		free(fc);
+
+	fprintf(
+		stderr,
+		"Skipping file due to error %d (%s), %s\n",
+		ret,
+		strerror(ret),
+		file->filename);
+
+	g_mutex_lock(mutex);
+	remove_hashed_blocks(tree, file);
+	/*
+	 * filerec_free will remove from the filerec tree keep it
+	 * under tree_mutex until we have a need for real locking in
+	 * filerec.c
+	 */
+	filerec_free(file);
+	g_mutex_unlock(mutex);
+
+	return;
+}
+
+int populate_tree_aim(struct hash_tree *tree)
+{
+	int ret = 0;
+	struct filerec *file, *tmp;
+	GMutex mutex;
+	GError *err = NULL;
+	GThreadPool *pool;
+
+	g_mutex_init(&mutex);
+	g_dataset_set_data_full(tree, "mutex", &mutex,
+				(GDestroyNotify) g_mutex_clear);
+
+	pool = g_thread_pool_new((GFunc) csum_whole_file, tree, io_threads,
+				 FALSE, &err);
+	if (err != NULL) {
+		fprintf(
+			stderr,
+			"Unable to create thread pool: %s\n",
+			err->message);
+		ret = -1;
+		g_error_free(err);
+		err = NULL;
+		goto out;
+	}
+
+	printf("Using %u threads for file hashing phase\n", io_threads);
+
+	list_for_each_entry_safe(file, tmp, &filerec_list, rec_list) {
+		g_thread_pool_push(pool, file, &err);
+		if (err != NULL) {
+			fprintf(stderr,
+					"g_thread_pool_push: %s\n",
+					err->message);
+			g_error_free(err);
+			err = NULL;
+		}
+	}
+
+	g_thread_pool_free(pool, FALSE, TRUE);
+out:
+	g_dataset_remove_data(tree, "mutex");
+
+	return ret;
+}
+
+static void csum_whole_file_swap(struct filerec *file, struct thread_params *params)
+{
+	struct rb_root *tree = params->tree;
 	uint64_t off = 0;
 	ssize_t bytes = 0, bytes_read = 0;
 	int ret = 0;
@@ -249,6 +417,7 @@ static void csum_whole_file(struct filerec *file, struct thread_params *params)
 	assert(digest != NULL);
 	static long long unsigned cur_num_filerecs = 0;
 
+	struct d_tree *d_tree;
 	GMutex *mutex = g_dataset_get_data(params, "mutex");
 
 	__sync_add_and_fetch(&cur_num_filerecs, 1);
@@ -313,62 +482,51 @@ static void csum_whole_file(struct filerec *file, struct thread_params *params)
 
 		checksum_block(buf, bytes, digest);
 
-		if (params->hfile == -1) {
-			/* All-in-memory path */
-			g_mutex_lock(mutex);
-			ret = insert_hashed_block(tree, digest, file, off, flags);
-			g_mutex_unlock(mutex);
-			if (ret)
-				break;
-		} else {
-			/* Swap-file path */
-			hashes = realloc(hashes, sizeof(struct block) * (nb_hash + 1));
-			hashes[nb_hash].loff = off;
-			hashes[nb_hash].flags = flags;
-			memcpy(hashes[nb_hash].digest, digest, DIGEST_LEN_MAX);
-			nb_hash++;
-		}
+		hashes = realloc(hashes, sizeof(struct block) * (nb_hash + 1));
+		hashes[nb_hash].loff = off;
+		hashes[nb_hash].flags = flags;
+		memcpy(hashes[nb_hash].digest, digest, DIGEST_LEN_MAX);
+		nb_hash++;
 
 		off += bytes;
 		bytes = 0;
 	}
 
-	if (params->hfile != -1) {
-		/* swap-file path taken:
-		 * write down all hashes, add all hashes to the bloom filter,
-		 * and store possibly dups
-		 */
-		g_mutex_lock(mutex);
-		file->num_blocks = nb_hash;
-		ret = write_file_info(params->hfile, file);
-		if (ret)
-			goto err;
+	/* swap-file path taken:
+	 * write down all hashes, add all hashes to the bloom filter,
+	 * and store possibly dups
+	 */
+	g_mutex_lock(mutex);
+	file->num_blocks = nb_hash;
+	ret = write_file_info(params->hfile, file);
+	if (ret)
+		goto err;
 
-		for (i = 0; i < nb_hash; i++) {
-			ret = bloom_add(&params->bloom,
-				hashes[i].digest, DIGEST_LEN_MAX);
-			if (ret == 1) {
-				ret = insert_hashed_block(tree, hashes[i].digest,
-						file, hashes[i].loff, hashes[i].flags);
-				if (ret)
-					goto err;
-				params->bloom_match++;
-				matched++;
-			}
-
-			ret = write_one_hash(params->hfile, hashes[i].loff,
-					hashes[i].flags, hashes[i].digest);
+	for (i = 0; i < nb_hash; i++) {
+		ret = bloom_add(&params->bloom,
+			hashes[i].digest, DIGEST_LEN_MAX);
+		if (ret == 1) {
+			d_tree = digest_new(hashes[i].digest);
+			digest_insert(tree, d_tree);
+			ret = 0;
 			if (ret)
 				goto err;
+			params->bloom_match++;
+			matched++;
 		}
 
-		file->num_blocks = matched;
-
-		params->num_files++;
-		params->num_hashes += nb_hash;
-
-		g_mutex_unlock(mutex);
+		ret = write_one_hash(params->hfile, hashes[i].loff,
+				hashes[i].flags, hashes[i].digest);
+		if (ret)
+			goto err;
 	}
+
+	file->num_blocks = matched;
+
+	params->num_files++;
+	params->num_hashes += nb_hash;
+
+	g_mutex_unlock(mutex);
 
 	filerec_close(file);
 	free(digest);
@@ -395,7 +553,6 @@ err_noclose:
 		file->filename);
 
 	g_mutex_lock(mutex);
-	remove_hashed_blocks(tree, file);
 	/*
 	 * filerec_free will remove from the filerec tree keep it
 	 * under tree_mutex until we have a need for real locking in
@@ -407,7 +564,8 @@ err_noclose:
 	return;
 }
 
-int populate_hash_tree(struct hash_tree *tree, char* serialize_fname)
+
+int populate_tree_swap(struct rb_root *tree, char* serialize_fname)
 {
 	int ret = 0;
 	struct filerec *file, *tmp;
@@ -417,26 +575,22 @@ int populate_hash_tree(struct hash_tree *tree, char* serialize_fname)
 
 	struct thread_params params = { tree, 0, 0, 0, };
 
-	if (serialize_fname) {
-		params.hfile = open(serialize_fname, O_WRONLY|O_CREAT|O_TRUNC, 0644);
+	params.hfile = open(serialize_fname, O_WRONLY|O_CREAT|O_TRUNC, 0644);
 
-		/* Write a dummy header */
-		ret = write_header(params.hfile, 0, 0, blocksize);
-		if (ret)
-			goto out;
+	/* Write a dummy header */
+	ret = write_header(params.hfile, 0, 0, blocksize);
+	if (ret)
+		goto out;
 
-		ret = bloom_init(&params.bloom, walked_size / blocksize, 0.01);
-		if (ret)
-			goto out;
-	} else {
-		params.hfile = -1;
-	}
+	ret = bloom_init(&params.bloom, walked_size / blocksize, 0.01);
+	if (ret)
+		goto out;
 
 	g_mutex_init(&mutex);
 	g_dataset_set_data_full(&params, "mutex", &mutex,
 				(GDestroyNotify) g_mutex_clear);
 
-	pool = g_thread_pool_new((GFunc) csum_whole_file, &params, io_threads,
+	pool = g_thread_pool_new((GFunc) csum_whole_file_swap, &params, io_threads,
 				 FALSE, &err);
 	if (err != NULL) {
 		fprintf(
@@ -464,22 +618,21 @@ int populate_hash_tree(struct hash_tree *tree, char* serialize_fname)
 
 	g_thread_pool_free(pool, FALSE, TRUE);
 
-	if (serialize_fname) {
-		/* Now, write the real header */
-		ret = write_header(params.hfile, params.num_files,
-				params.num_hashes, blocksize);
-		if (ret)
-			goto out;
+	/* Now, write the real header */
+	ret = write_header(params.hfile, params.num_files,
+			params.num_hashes, blocksize);
+	if (ret)
+		goto out;
 
-		printf("Bloom gave us %i hashes as 'almost duplicate'\n",
-			params.bloom_match);
-	}
+	printf("Bloom gave us %i hashes as 'almost duplicate'\n",
+		params.bloom_match);
+	int count = digest_count(tree);
+	printf("We stored %i unique hashes\n", count);
+	printf("bloom error rate: %.2f%%\n", (1 - ((float)count / (float)params.bloom_match)) * 100);
 
 out:
-	if (serialize_fname) {
-		bloom_free(&params.bloom);
-		close(params.hfile);
-	}
+	bloom_free(&params.bloom);
+	close(params.hfile);
 	g_dataset_destroy(&params);
 
 	return ret;
