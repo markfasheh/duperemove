f308ed881ec646822714404b511f6dffaa6b2ff8
Mark Fasheh <mfasheh@suse.de>
2015-06-04 17:51:44 -0700

    Align dedupe request length if we fail to dedupe from the unaligned length. This provides us backwards compatibilty with kernels that require a block aligned dedupe length.
    Signed-off-by: Mark Fasheh <mfasheh@suse.de>
    Git-commit: 9430adc

--- dedupe.c
+++ dedupe.c
@@ -19,6 +19,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+#include <sys/vfs.h>
+
+#include <errno.h>
 
 #include "kernel.h"
 #include "list.h"
@@ -27,6 +30,8 @@
 #include "debug.h"
 
 #define MAX_DEDUPES_PER_IOCTL	120
+
+static int must_align_len = 0;
 
 struct dedupe_req {
 	struct filerec		*req_file;
@@ -136,6 +141,17 @@ void free_dedupe_ctxt(struct dedupe_ctxt *ctxt)
 	}
 }
 
+static unsigned int get_fs_blocksize(struct filerec *file)
+{
+	int ret;
+	struct statfs fs;
+
+	ret = fstatfs(file->fd, &fs);
+	if (ret)
+		return 0;
+	return fs.f_bsize;
+}
+
 struct dedupe_ctxt *new_dedupe_ctxt(unsigned int max_extents, uint64_t loff,
 				    uint64_t elen, struct filerec *ioctl_file)
 {
@@ -171,6 +187,13 @@ struct dedupe_ctxt *new_dedupe_ctxt(unsigned int max_extents, uint64_t loff,
 	INIT_LIST_HEAD(&ctxt->queued);
 	INIT_LIST_HEAD(&ctxt->in_progress);
 	INIT_LIST_HEAD(&ctxt->completed);
+
+	ctxt->fs_blocksize = get_fs_blocksize(ioctl_file);
+	if (!ctxt->fs_blocksize) {
+		free(same);
+		free(ctxt);
+		return NULL;
+	}
 
 	return ctxt;
 }
@@ -212,6 +235,14 @@ static void add_dedupe_request(struct dedupe_ctxt *ctxt,
 		(unsigned long long)req->req_loff, same->dest_count);
 }
 
+static void set_aligned_same_length(struct dedupe_ctxt *ctxt,
+				    struct btrfs_ioctl_same_args *same)
+{
+	same->length = ctxt->len;
+	if (must_align_len && ctxt->len > ctxt->fs_blocksize)
+		same->length = ctxt->len & ~(ctxt->fs_blocksize - 1);
+}
+
 static void populate_dedupe_request(struct dedupe_ctxt *ctxt,
 				    struct btrfs_ioctl_same_args *same)
 {
@@ -219,7 +250,7 @@ static void populate_dedupe_request(struct dedupe_ctxt *ctxt,
 
 	memset(same, 0, ctxt->same_size);
 
-	same->length = ctxt->len;
+	set_aligned_same_length(ctxt, same);
 	same->logical_offset = ctxt->ioctl_file_off;
 
 	list_for_each_entry_safe(req, tmp, &ctxt->queued, req_list) {
@@ -279,12 +310,19 @@ int dedupe_extents(struct dedupe_ctxt *ctxt)
 		/* Convert the queued list into an actual request */
 		populate_dedupe_request(ctxt, ctxt->same);
 
+retry:
 		ret = btrfs_extent_same(ctxt->ioctl_file->fd, ctxt->same);
 		if (ret)
 			break;
 
 		if (debug)
 			print_btrfs_same_info(ctxt);
+
+		if (ctxt->same->info[0].status == -EINVAL && !must_align_len) {
+			must_align_len = 1;
+			set_aligned_same_length(ctxt, ctxt->same);
+			goto retry;
+		}
 
 		process_dedupes(ctxt, ctxt->same);
 	}
