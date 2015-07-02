13e57efb3e87772bd407830dd53d9a7f220b979a
Mark Fasheh <mfasheh@suse.de>
2015-07-01 21:47:05 -0700

    ioctl info print should be debug not verbose
    Signed-off-by: Mark Fasheh <mfasheh@suse.de>

--- dedupe.c
+++ dedupe.c
@@ -232,7 +232,7 @@ static void add_dedupe_request(struct dedupe_ctxt *ctxt,
 	info->bytes_deduped = 0;
 	same->dest_count++;
 
-	vprintf("add ioctl request %s, off: %llu, dest: %d\n", file->filename,
+	dprintf("add ioctl request %s, off: %llu, dest: %d\n", file->filename,
 		(unsigned long long)req->req_loff, same->dest_count);
 }
 
