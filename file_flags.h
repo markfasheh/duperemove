0f8456aebd45f004d2fa223006ac3dcef4696acd
Alexandre Bruyelles <git@jack.fr.eu.org>
2023-11-12 16:27:32 +0100

    file_scan: use a per-file flag to identity inlined files
    Signed-off-by: Alexandre Bruyelles <git@jack.fr.eu.org>

--- file_flags.h
+++ file_flags.h
@@ -1,7 +1,16 @@
 #ifndef __FILE_FLAGS_H__
 #define __FILE_FLAGS_H__
 
-/* Fiemap flags that would cause us to skip comparison of the block */
+/* Fiemap flags that would cause us to mark the extent as undeduplicable */
 #define FIEMAP_SKIP_FLAGS	(FIEMAP_EXTENT_DATA_INLINE|FIEMAP_EXTENT_UNWRITTEN)
+
+/*
+ * The following flags may be used in the hashfile.
+ * Do not change the values recklessly.
+ */
+/* File is inlined. We store no extents nor hashes for it.
+ * We should not try to deduplicate this file, won't work anyway.
+ */
+#define FILE_INLINED		0x0001
 
 #endif
